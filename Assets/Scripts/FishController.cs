abfaf036ea068633a0248dd1016fda931dadb899
hazed <hazed@syneforge.com>
2020-03-20 12:36:44 +0200

    added fish controller

---
+++ Assets/Scripts/FishController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishController : MonoBehaviour {
+
+  public Transform _fish;
+  public float _speed = 10.0f;
+  public float _rotationSpeed = 100.0f;
+
+  // Start is called before the first frame update
+  void Start() {
+
+  }
+
+  // Update is called once per frame
+  void Update() {
+    float translation = Input.GetAxis("Vertical") * _speed;
+    float rotation = Input.GetAxis("Horizontal") * _rotationSpeed;
+
+    // Make it move 10 meters per second instead of 10 meters per frame...
+    translation *= Time.deltaTime;
+    rotation *= Time.deltaTime;
+
+    // Move translation along the object's z-axis
+    // var position = _fish.position;
+    // position.x += translation;
+    // _fish.position = position;
+    _fish.Translate(translation, 0, 0);
+
+    // Rotate around our y-axis
+    _fish.Rotate(0, rotation, 0);
+  }
+}
